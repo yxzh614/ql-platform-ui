3073b6fd82c9db9b0317f2423845e563108939ef
yxzh614 <526686022@qq.com>
2019-05-13 17:58:07 +0800

    1

--- src/components/CheckBox/index.tsx
+++ src/components/CheckBox/index.tsx
@@ -10,25 +10,28 @@ export interface CheckboxProps {
     onChange: (e: React.MouseEvent, checked: boolean) => void
 }
 
-export default function (props: CheckboxProps) {
+export default class extends React.Component<CheckboxProps> {
     // const [check, setCount] = useState(false)
-    const {
-        onChange,
-        label,
-        checked
-    } = props
-    return (
-        <div 
-        className={'ql-checkbox'} 
-        onClick = {(e) => {onChange && onChange(e, !checked)}}
-        >
-            <div className={
-                classnames('ql-checkbox-inner',
-                checked ? 'checked' : '')
-            }>
-                <Icon type={'check'} />
+    render () {
+        const {
+            onChange,
+            label,
+            checked
+        } = this.props
+        
+        return (
+            <div 
+            className={'ql-checkbox'} 
+            onClick = {(e) => {onChange && onChange(e, !checked)}}
+            >
+                <div className={
+                    classnames('ql-checkbox-inner',
+                    checked ? 'checked' : '')
+                }>
+                    <Icon type={'check'} />
+                </div>
+                <span className={'ql-checkbox-label'}>{label}</span>
             </div>
-            <span className={'ql-checkbox-label'}>{label}</span>
-        </div>
-    )
+        )
+    }
 }
