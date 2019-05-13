9bcefb38ef950f928679a53ae6a5253341bd31be
yxzh614 <526686022@qq.com>
2019-05-13 17:52:09 +0800

    1

--- src/components/CheckBox/index.tsx
+++ src/components/CheckBox/index.tsx
@@ -1,5 +1,34 @@
-import Checkbox from './Checkbox.tsx'
-import CheckboxPrpos from './Checkbox.tsx'
-import CheckboxGroup from './CheckboxGroup.tsx'
+import React from 'react'
+// import {useState} from 'react'
+import Icon from '../Icon'
+import './main.less'
+import classnames from 'classnames'
 
-export default Checkbox
+export interface CheckboxProps {
+    checked: boolean
+    label: string
+    onChange: (e: React.MouseEvent, checked: boolean) => void
+}
+
+export default function (props: CheckboxProps) {
+    // const [check, setCount] = useState(false)
+    const {
+        onChange,
+        label,
+        checked
+    } = props
+    return (
+        <div 
+        className={'ql-checkbox'} 
+        onClick = {(e) => {onChange && onChange(e, !checked)}}
+        >
+            <div className={
+                classnames('ql-checkbox-inner',
+                checked ? 'checked' : '')
+            }>
+                <Icon type={'check'} />
+            </div>
+            <span className={'ql-checkbox-label'}>{label}</span>
+        </div>
+    )
+}
